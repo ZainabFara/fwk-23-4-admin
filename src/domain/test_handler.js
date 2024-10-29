98ec67a83ae9aa5f3f620f356957621d990e40e9
sarah <sarah.emmoth@proton.me>
2024-10-29 13:14:33 +0100

    Working test routes

--- src/domain/test_handler.js
+++ src/domain/test_handler.js
@@ -1,39 +1,73 @@
-const mysql = require('mysql2');
-const { v4: uuidv4 } = require('uuid');
+const mysql = require("mysql2");
+const { v4: uuidv4 } = require("uuid");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'test',
-    password: 'test',
-    database: 'test'
+  host: "localhost",
+  user: "test",
+  password: "test",
+  database: "test",
 });
 
 const addUser = (user) => {
-    user.id = uuidv4(); 
-    return new Promise((resolve, reject) => {
-        db.query(
-            "INSERT INTO users (name, password, email, uuid) VALUES (?, ?, ?, ?)", 
-            [user.name, user.password, user.email, user.id], 
-            (err, results) => {
-                if (err) return reject(err);
-                resolve({ ...user }); 
-            }
-        );
-    });
+  user.id = uuidv4();
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO users (name, password, email, uuid) VALUES (?, ?, ?, ?)",
+      [user.name, user.password, user.email, user.id],
+      (err, results) => {
+        if (err) return reject(err);
+        resolve({ ...user });
+      }
+    );
+  });
 };
 
 addUser({ name: "sarah", password: "test", email: "test@test.se" })
-    .then((createdUser) => {
-        console.log("User created:", createdUser); 
-    })
-    .catch((error) => {
-        console.error("Error creating user:", error); 
+  .then((createdUser) => {
+    console.log("User created:", createdUser);
+  })
+  .catch((error) => {
+    console.error("Error creating user:", error);
+  });
+
+getUsers = () => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users", (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
     });
+  });
+};
+
+const getUser = (name) => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users WHERE name = ?", [name], (err, results) => {
+      if (err) return reject(err);
+      if (results.length > 0) {
+        resolve(results[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+
+getUser("sarah").then((user) => {
+  if (user) {
+    console.log("User found:", user);
+  } else {
+    console.log("No user found with that name.");
+  }
+});
+
+getUsers().then((users) => {
+  console.log("All users:", users);
+});
 
 db.end((err) => {
-    if (err) {
-        console.error("Error closing the connection:", err);
-    } else {
-        console.log("Database connection closed.");
-    }
+  if (err) {
+    console.error("Error closing the connection:", err);
+  } else {
+    console.log("Database connection closed.");
+  }
 });
--- src/domain/user_handler.js
+++ src/domain/user_handler.js
@@ -1,131 +1,141 @@
-const { v4: uuidv4 } = require('uuid');
-const mysql = require('mysql2');
+const { v4: uuidv4 } = require("uuid");
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: 'localhost',    
-    user: 'test', 
-    password: 'test', 
-    database: 'test'   
+  host: "localhost",
+  user: "test",
+  password: "test",
+  database: "test",
 });
 
 db.connect((err) => {
-    if(err) {
-        console.error("Error connecting to database!", err);
-        return;
-    }
-    console.log("Connected to database");
+  if (err) {
+    console.error("Error connecting to database!", err);
+    return;
+  }
+  console.log("Connected to database");
 });
 
 exports.addUser = (user) => {
-    user.id = uuidv4();
-    return new promise((resolve, reject) => {
-        db.query(
-            "INSERT INTO users(name, password, email) VALUES(?, ?, ?)",
-            [user.name , user.password, user.email],
-            (err, results) => {
-                if(err) return reject(err);
-                resolve({...user, id});
-            }
-        );
-    });
+  user.id = uuidv4();
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO users(name, password, email, uuid) VALUES(?, ?, ?, ?)",
+      [user.name, user.password, user.email, user.id],
+      (err, results) => {
+        if (err) return reject(err);
+        resolve({ ...user });
+      }
+    );
+  });
 };
 
 exports.getUsers = () => {
-    return users.map(user => ({
-        ...user,
-        groups: getUserGroups(user.id)
-    }));
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users", (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
 };
 
-exports.getUser = (id) => {
-    return users.find(user => user.id === id);
+exports.getUser = (name) => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users WHERE name = ?", [name], (err, results) => {
+      if (err) return reject(err);
+      if (results.length > 0) {
+        resolve(results[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
 };
-
 
 exports.updateUser = (id, newUserDetails) => {
-    let userIndex = users.findIndex(user => user.id === id);
-    if (userIndex !== -1) {
-        users[userIndex] = {...users[userIndex], ...newUserDetails};
-        return true;
-    }
-    return false;
+  let userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    users[userIndex] = { ...users[userIndex], ...newUserDetails };
+    return true;
+  }
+  return false;
 };
 
-
-exports.deleteUser = (id) => {
-    let initialLength = users.length;
-    users = users.filter(user => user.id === id);  // Removed parseInt
-    return initialLength !== users.length;
+exports.deleteUser = (name) => {
+    return new Promise((resolve, reject) => {
+        db.query("DELETE FROM users WHERE name = ?", [name], (err, results) => {
+          if (err) return reject(err);
+        });
+      });
 };
 
 exports.addGroup = (groupName) => {
-    if (!groups[groupName]) {
-        groups[groupName] = [];
-        return true;
-    }
-    return false;
+  if (!groups[groupName]) {
+    groups[groupName] = [];
+    return true;
+  }
+  return false;
 };
 
 exports.listGroups = () => {
-    return Object.keys(groups).map(groupName => ({
-        name: groupName,
-        members: groups[groupName].map(userId =>
-            this.getUser(userId)
-        )
-    }));
+  return Object.keys(groups).map((groupName) => ({
+    name: groupName,
+    members: groups[groupName].map((userId) => this.getUser(userId)),
+  }));
 };
 
 exports.deleteGroup = (groupName) => {
-    if (groups.hasOwnProperty(groupName)) {
-        delete groups[groupName];
-        return true;
-    }
-    return false;
+  if (groups.hasOwnProperty(groupName)) {
+    delete groups[groupName];
+    return true;
+  }
+  return false;
 };
 
 const getUserGroups = (userId) => {
-    return Object.keys(groups).filter(groupName => groups[groupName].includes(userId));
+  return Object.keys(groups).filter((groupName) =>
+    groups[groupName].includes(userId)
+  );
 };
 
 exports.addUserToGroup = (userId, groupName) => {
-    if (!groups[groupName]) {
-        console.log("Group does not exist.");
-        return false;
-    }
+  if (!groups[groupName]) {
+    console.log("Group does not exist.");
+    return false;
+  }
 
-    const userExists = users.some(user => user.id === userId);
-    if (!userExists) {
-        console.log("User does not exist.");
-        return false;
-    }
+  const userExists = users.some((user) => user.id === userId);
+  if (!userExists) {
+    console.log("User does not exist.");
+    return false;
+  }
 
-    if (groups[groupName].includes(userId)) {
-        console.log("User already in the group.");
-        return false;
-    }
+  if (groups[groupName].includes(userId)) {
+    console.log("User already in the group.");
+    return false;
+  }
 
-    groups[groupName].push(userId);
-    return true;
+  groups[groupName].push(userId);
+  return true;
 };
 
-
 exports.removeUserFromGroup = (userId, groupName) => {
-    if (groups[groupName]) {
-        let initialLength = groups[groupName].length;
-        groups[groupName] = groups[groupName].filter(id => id !== userId);
-        return initialLength !== groups[groupName].length;
-    }
-    return false;
+  if (groups[groupName]) {
+    let initialLength = groups[groupName].length;
+    groups[groupName] = groups[groupName].filter((id) => id !== userId);
+    return initialLength !== groups[groupName].length;
+  }
+  return false;
 };
 
 exports.getUserByUsername = (username) => {
-    return users.find(user => user.username === username);
+  return users.find((user) => user.username === username);
 };
 
 db.end((err) => {
-    if (err) {
-        console.error("Error closing the connection:", err);
-    } else {
-        console.log("Database connection closed.");
-    }
+  if (err) {
+    console.error("Error closing the connection:", err);
+  } else {
+    console.log("Database connection closed.");
+  }
 });
